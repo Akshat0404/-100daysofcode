848a8184a0210cacf83fc88f40b332baf21e2f88
Akshat Roy <91226038+Akshat0404@users.noreply.github.com>
2023-05-19 22:57:23 +0530

    Create day10 climbingstairs.cpp

---
+++ day10 climbingstairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2)
+            return n;
+        
+        int prev1 = 1;
+        int prev2 = 2;
+        int current = 0;
+        
+        for (int i = 3; i <= n; ++i) {
+            current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+        
+        return current;
+    }
+};
