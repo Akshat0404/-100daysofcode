01af269734fb3b428fe11ab8cc3c7242277314f3
Akshat Roy <91226038+Akshat0404@users.noreply.github.com>
2023-08-01 23:08:30 +0530

    Create day53 happynumber.cpp

---
+++ day53 happynumber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+        while (n != 1 && !seen.count(n)) {
+            seen.insert(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+};
