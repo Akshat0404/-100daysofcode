a77936a175bc7b980914e2606607aeb75ca9279c
Akshat Roy <91226038+Akshat0404@users.noreply.github.com>
2023-12-14 23:37:53 +0530

    Create day75 movezeroes.cpp

---
+++ day75 movezeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(std::vector<int>& nums) {
+        int nonZeroIndex = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
+            }
+        }
+
+        for (int i = nonZeroIndex; i < nums.size(); ++i) {
+            nums[i] = 0;
+        }
+    }
+};
