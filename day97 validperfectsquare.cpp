4b7e383354cdfb3fb1cae4f8a9fbb7c24ffd5fb0
Akshat Roy <91226038+Akshat0404@users.noreply.github.com>
2023-12-30 21:17:03 +0530

    Create day97 validperfectsquare.cpp

---
+++ day97 validperfectsquare.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long left = 1, right = num;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
